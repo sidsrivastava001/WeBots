33f5edb0de6376798e33ebd502a776dc91e8ad55
sidsrivastava001 <49328871+sidsrivastava001@users.noreply.github.com>
2020-08-25 22:26:37 -0400

    first commit

---
+++ controllers/my_controller/build/release/my_controller.d
@@ -0,0 +1,7 @@
+build/release/my_controller.o: my_controller.cpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Robot.hpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Motor.hpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Device.hpp \
+ C:/Program\ Files/Webots/include/controller/c/webots/types.h \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/PositionSensor.hpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/DistanceSensor.hpp
