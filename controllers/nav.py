51cfce8ddd32878c0b4220fda0afdc2fb0b839e1
Chris Gu <cgu2022@pingry.org>
2020-08-29 03:52:15 -0400

    Got BFS to work

--- controllers/nav.py
+++ controllers/nav.py
@@ -1,79 +1,298 @@
+import os.path
+
+
 class cell:
     def __init__(self):
         self.visited = False
-        self.wall = [False, False, False, False] # Up, Right, Down, Left
+        self.wall = [False, False, False, False]  # Up, Right, Down, Left
         self.victim = False
         self.checkpoint = False
-    
+
     def blackout(self):
-        self.wall = [True, True, True, True] # All walls up
+        self.wall = [True, True, True, True]  # All walls up
         self.visited = True
-    
+
 
 class Nav:
-    startPosition = (10,10)
     def __init__(self):
         print("Initialized AI!")
-        # 2D array 
-        self.rows, self.cols = (20, 20) 
-        self.field = [[cell()]*self.cols]*self.rows
-        self.location = startPosition # row, column
-        self.previous = (-1, -1)
-        self.filePtr = open("wall.txt", 'r+')
-        if self.filePtr:    
-            print("file is opened successfully")
-        data = self.filePtr.read().split() # Read in whole file, and then turn string into list of words
+        # 2D array
+        self.initialPosition = (10, 10)
+        self.rows, self.cols = (20, 20)
+        #self.field = [[cell()]*self.cols]*self.rows
+        self.field = [[cell() for j in range(self.cols)]
+                      for i in range(self.rows)]
+        self.startPosition = self.initialPosition
+        # row, column. Robot's current position on the field
+        self.location = self.startPosition
+        self.direction = 0  # The current direction of the robot
+        self.previousLocation = (-1, -1)
+        if os.path.isfile('wall.txt'):
+            self.filePtr = open("wall.txt", 'r+')
+        else:  # If wall.txt does not exist
+            print("Creating wall.txt!")
+            self.filePtr = open('wall.txt', 'w+')
+        if self.filePtr:
+            print("Wall.txt is opened successfully")
+        else:
+            print("Wall.txt did not open successfully. . .")
+            raise SystemExit
+            quit()
+        # Read in whole file, and then turn string into list of words
+        data = self.filePtr.read().split()
         while len(data) > 0:
             row = int(data.pop(0))
             col = int(data.pop(0))
-            direction = data.pop(0)
-            print("Read in:", row, col, direction)
-            self.writeWall(direction, (row,col))
-    
-    def convertDirection(self, direction): # Converts a direction in str to int format or int to str format
+            typeOfData = data.pop(0)
+            print("Read in:", row, col, typeOfData)
+            if typeOfData == 'V':
+                print("Read visited!")
+                self.markVisited()
+            elif typeOfData == "VICTIM":
+                print("Read victim!")
+                self.field[row][col].victim = True
+            elif typeOfData == "CHECKPOINT":
+                print("Read checkpoint!")
+                self.field[row][col].checkpoint = True
+                # Update starting position to last found checkpoint
+                self.startPosition = (row, col)
+            else:  # Data is giving a wall direction
+                self.markWall(typeOfData, (row, col), writeToFile=False)
+        # mark victim at current location to make sure robot knows current tile is visited
+
+    # Converts a direction in str to int format or int to str format
+    def convertDirection(self, direction):
         if isinstance(direction, str):
-            if direction == 'UP':
+            if direction == 'NORTH':
                 return 0
-            if direction == 'RIGHT':
+            if direction == 'EAST':
                 return 1
-            if direction == 'DOWN':
+            if direction == 'SOUTH':
                 return 2
-            if direction == 'LEFT':
+            if direction == 'WEST':
                 return 3
         elif isinstance(direction, int):
             if direction == 0:
-                return 'UP'
+                return 'NORTH'
             if direction == 1:
-                return 'RIGHT'
+                return 'EAST'
             if direction == 2:
-                return 'DOWN'
+                return 'SOUTH'
             if direction == 3:
-                return 'LEFT'
+                return 'WEST'
         else:
             print("Direction Conversion Error!")
             raise SystemExit
             quit()
 
-    # This function takes the direction of the wall, enters it at a specified location [or the current location], and optionally writes the wall data to a file.
-    def writeWall(self, direction, loc=None, writeToFile=False): # str, [tuple], [boolean]
-        if loc is None: # Defualt loc to current location
-            loc = self.location
-        if isinstance(direction, str): # Convert direction to integer
-            direction = direction.upper() # uppercase everything to match formatting
-            direction = self.convertDirection(direction)
-        
-        self.field[loc[0]][loc[1]].wall[direction] = True # Mark Wall
+    # Converts a command in str to int format or int to str format
+    def convertCommand(self, command):
+        if isinstance(command, str):
+            if command == 'FORWARD':  # Move forward
+                return 0
+            if command == 'RIGHT':  # Turn right 90 deg, then move forward
+                return 1
+            if command == 'BACKWARD':  # Turn 180 deg, then move forward
+                return 2
+            # Turn left 90 deg (-90 deg), then move forward
+            if command == 'LEFT':
+                return 3
+        elif isinstance(command, int):
+            if command == 0:
+                return 'FORWARD'
+            if command == 1:
+                return 'RIGHT'
+            if command == 2:
+                return 'BACKWARD'
+            if command == 3:
+                return 'LEFT'
+        else:
+            print("Command Conversion Error!")
+            raise SystemExit
+            quit()
 
-        direction = self.convertDirection(direction) # Convert direction to string
+    # This function returns the correct relative command for the robot to traverse. Call this funtion BEFORE you update self.location during calculate()!
+    # @return Returns the needed command given circumstances and returns the new direction of the robot after executing this command
+    def determineCommand(self, currentDirection, newPosition, oldPosition=None):
+        if oldPosition is None:  # Defualt oldPosition to current location
+            oldPosition = self.location
+
+        # 1. Calculate the new needed direction
+        adjustRow = newPosition[0] - oldPosition[0]
+        adjustCol = newPosition[1] - oldPosition[1]
+        if adjustRow == 1:  # Moving up
+            newDirection = 0
+        elif adjustCol == 1:  # Moving right
+            newDirection = 1
+        elif adjustRow == -1:  # Moving down
+            newDirection = 2
+        else:  # Moving left
+            newDirection = 3
+
+        # 2. Calculate what command to adjust the current direction into the new direction
+        adjustDirection = ((newDirection-currentDirection) + 4) % 4
+        if adjustDirection == 0:
+            return 'FORWARD', newDirection
+        elif adjustDirection == 1:
+            return 'RIGHT', newDirection
+        elif adjustDirection == 2:
+            return 'BACKWARD', newDirection
+        else:
+            return 'LEFT', newDirection
+
+    # This function takes the direction of the wall, enters it at a specified location [or the current location], and optionally writes the wall data to a file.
+    # str, [tuple], [boolean]
+    def markWall(self, direction, loc=None, writeToFile=True):
+        if loc is None:  # Defualt loc to current location
+            loc = self.location
+        if isinstance(direction, str):  # Convert direction to integer
+            direction = direction.upper()  # uppercase everything to match formatting
+            direction = self.convertDirection(direction)
+
+        self.field[loc[0]][loc[1]].wall[direction] = True  # Mark Wall
+
+        direction = self.convertDirection(direction)  # Convert direction to string
         if writeToFile:
-            self.filePtr.write(str(loc[0])+ ' ' + str(loc[1]) + ' ' + direction + '\n') # Row, Col, Direction
+            # Row, Col, Direction
+            self.filePtr.write(str(loc[0]) + ' ' +
+                               str(loc[1]) + ' ' + direction + '\n')
             print("Entered & wrote wall: ", direction)
         else:
             print("Entered wall: ", direction)
 
+    def markCheckpoint(self, loc=None):
+        if loc is None:  # Defualt loc to current location
+            loc = self.location
+        print("Wrote Checkpoint to File!")
+        # Row, Col, Direction
+        self.filePtr.write(str(loc[0]) + ' ' + str(loc[1]) + ' ' + 'CHECKPOINT' + '\n')
+        self.field[loc[0]][loc[1]].checkpoint = True  # Mark Victim
+
+    def markVisited(self, loc=None):
+        if loc is None:  # Defualt loc to current location
+            loc = self.location
+        print("Wrote Visited to File!")
+        # Row, Col, Direction
+        self.filePtr.write(str(loc[0]) + ' ' + str(loc[1]) + ' ' + 'V' + '\n')
+        self.field[loc[0]][loc[1]].visited = True  # Mark Victim
+
+    # Marks seen victim at current location
+    def markVictim(self, loc=None):
+        if loc is None:  # Defualt loc to current location
+            loc = self.location
+        print("Wrote Victim to File!")
+        # Row, Col, Direction
+        self.filePtr.write(str(loc[0]) + ' ' +
+                           str(loc[1]) + ' ' + 'VICTIM' + '\n')
+        self.field[loc[0]][loc[1]].victim = True  # Mark Victim
+
     # Function if want to move up
-    # @return: if True, then successfully moved
-    def moveUp(self, backtrack=False): #[boolean] if robot is backtracking (checking if tile is visited or not)
-        loc = self.location
-        if self.field[loc[0]][loc[1]].up:
-            return False
+    # if backtrack = true, only returns if you can or can't move up. If you can, then also returns coordinates
+    # @return: if True, then moved will be successfull. Will then also return new coordinates. if False, then move will not be allowed.
+    def canMove(self, direction, loc=None, backtrack=False):
+        if loc is None:  # Defualt loc to current location
+            loc = self.location
+        if isinstance(direction, str):  # Convert direction to integer
+            direction = direction.upper()  # uppercase everything to match formatting
+            direction = self.convertDirection(direction)
+
+        # Convert direction to adjust row & adjust col
+        adjustRow = 0
+        adjustCol = 0
+        if direction == 0:  # Moving up
+            adjustRow = 1
+        elif direction == 1:  # Moving right
+            adjustCol = 1
+        elif direction == 2:  # Moving down
+            adjustRow = -1
+        else:  # Moving left
+            adjustCol = -1
+
+        newLoc = (loc[0] + adjustRow, loc[1] + adjustCol)  # Row, Col
+        # If coordinates are out of bounds
+        if newLoc[0] < 0 or newLoc[0] >= self.rows or newLoc[1] < 0 or newLoc[1] >= self.cols:
+            return False, None
+        # If there is a wall in the direction trying to go
+        if self.field[loc[0]][loc[1]].wall[direction]:
+            return False, None
+        # If robot isn't backtracking but the new tile is already visited
+        if (not backtrack) and self.field[newLoc[0]][newLoc[1]].visited:
+            return False, None
+        return True, newLoc
+
+    def calculate(self):
+        newDirection, newLocation, foundMove, commands = 1, (-1, -1), False, list()  # Default values
+        for direction in range(0, 4):
+            moveIsPossible, newLocation = self.canMove(
+                direction, backtrack=False)
+            if moveIsPossible:
+                command, newDirection = self.determineCommand( self.direction, newLocation)
+                commands.append(command)
+                foundMove = True
+                break
+
+        if not foundMove:  # Need to BFS
+            commands, newLocation, newDirection = self.backtrackBFS()
+
+        self.direction = newDirection  # Update Direction
+        self.previousLocation = self.location  # Update previous location
+        self.location = newLocation  # Update location to new location
+        self.markVisited(newLocation)  # Mark new location as visited
+
+        # if commands == None: # Then backtrack home
+        return commands
+
+    # Finds the path to the nearest unvisited tile via BFS
+    # @return Returns the set of commands to reach the new location, the new location's coordinates, and the new direction. Does NOT update location & direction for you
+    def backtrackBFS(self):
+        print("BFS Initiated!")
+        # visited = [[False]*self.cols]*self.rows  # Visited array
+        visited = [[False for j in range(self.cols)] for i in range(self.rows)]
+        # prevCell = [(-1, -1)*self.cols]*self.rows  # Previous cell array
+        prevCell = [[[-1, -1] for j in range(self.cols)] for i in range(self.rows)]
+        queue = list()
+        queue.append(self.location)  # first add current location
+        targetLocation = None
+        while len(queue) > 0 and targetLocation == None:
+            currentCell = queue.pop(0)
+            visited[currentCell[0]][currentCell[1]] = True
+            for direction in range(0, 4):
+                moveIsPossible, newCell = self.canMove(direction, currentCell, backtrack=True)
+                if moveIsPossible:
+                    # if new cell is unvisited and possible to access
+                    if self.field[newCell[0]][newCell[1]].visited == False:
+                        prevCell[newCell[0]][newCell[1]] = (currentCell[0], currentCell[1])
+                        targetLocation = newCell
+                        break
+                    # new cell is not in the queue and new cell has not been visited yet
+                    elif (not newCell in queue) and visited[newCell[0]][newCell[1]] == False:
+                        prevCell[newCell[0]][newCell[1]] = (currentCell[0], currentCell[1])
+                        queue.append(newCell)
+                    else:
+                        pass
+
+        if targetLocation == None:  # No more unvisited tiles
+            return None  # return none to indicate go back home
+
+        # Now it's time to backtrack each location! One by One!
+        locations = [targetLocation]
+        newPosition = list(targetLocation)
+        while newPosition != self.location:
+            newPosition = prevCell[newPosition[0]][newPosition[1]]
+            locations.append(newPosition)
+        locations.reverse()
+
+        # With each tile's coordinates on the path, we can now calculate the commands
+        commands = list()
+        currentPosition = locations[0]
+        currentDirection = self.direction
+        i = 1
+        while i < len(locations):
+            newPosition = locations[i]
+            command, newDirection = self.determineCommand(currentDirection, newPosition, oldPosition=currentPosition)
+            commands.append(command)
+            currentPosition = newPosition
+            currentDirection = newDirection
+            i += 1
+
+        return commands, currentPosition, currentDirection
--- controllers/tester.py
+++ controllers/tester.py
@@ -2,4 +2,22 @@ from nav import Nav
 
 AI = Nav()
 #print(AI.location)
-AI.writeWall("UP", (15,15), writeToFile=True)
+#AI.markWall(3)
+#AI.markVisited((4,4))
+x = AI.calculate()
+print(x)
+'''
+ 0,0 --> 1,0
+UP
+1. blackout() --> void
+2. Undo action
+3. calculate() --> will give you a list of commands
+4. proceed
+
+when you call black out
+1. wall off dest., mark as visited
+2. Revert back to previous position (0,0)
+2. calculate(): calculate how to go to next closest unvisited square
+3. Return a set of commands
+
+'''
